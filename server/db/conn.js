13bf3c4ed896811209459574d4d57484d78e5d0b
Chance Goodwin <cgood1413@gmail.com>
2022-03-29 23:42:08 -0500

    MongoDB MERN Walkthrough

---
+++ server/db/conn.js
@@ -0,0 +1,21 @@
+const {MongoClient} = require('mongodb');
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let _db;
+
+module.exports = {
+    connectToServer: callback => {
+        client.connect((err, db) => {
+            if (db) {
+                _db = db.db('employees');
+                console.log('Successfully connected to MongoDB.');
+            }
+            return callback(err);
+        });
+    },
+    getDb: () => {return _db},
+};
